65b2ef115f4383fba577c0e2d87ca3acefdce6e9
Kyle Robinson Young <kyle@dontkry.com>
2018-10-28 16:48:24 -0700

    Add types

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "locastore" {
+  export default class Locastore {
+    constructor(namespace?: string);
+    get(key: string): any;
+    set(key: string, value: any);
+    delete(key: string);
+    clear();
+  }
+  export function clear();
+}
